7cae0332ea77f770f691eb03aa91c6d4db37ad05
maryam karimi <maryamkarimi1376.71@gmail.com>
2023-03-16 18:10:11 +0330

    add my main class

---
+++ src/main/java/ir/maktab/finalprojectphase3/Finalprojectphase3Application.java
@@ -0,0 +1,15 @@
+package ir.maktab.finalprojectphase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class Finalprojectphase3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Finalprojectphase3Application.class, args);
+    }
+
+}
